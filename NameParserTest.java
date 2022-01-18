eaae495eaa18cf03450f4dd869eb75f87c365edf
unknown <serzhsnigur@gmail.com>
2022-01-18 17:24:31 +0200

    first commit

---
+++ NameParserTest.java
@@ -0,0 +1,25 @@
+class NameParser {
+    public String parse(String[] names) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < names.length; i++) {
+            String firstName = names[i].split(" ")[0];
+            builder.append(firstName);
+
+            if (i != names.length - 1) {
+                builder.append(", ");
+            }
+        }
+        String result = builder.toString();
+        return result;
+    }
+}
+
+class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
